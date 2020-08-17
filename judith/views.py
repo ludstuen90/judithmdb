a4fc9bbac428b055e4f390757fe63b9c6420aa62
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2020-08-17 15:40:01 -0300

    initial commit of portfolio section, in progress

--- judith/views.py
+++ judith/views.py
@@ -1,13 +1,39 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from judith.models import PortfolioItem
 
-# Create your views here.
-
-
-from django.http import HttpResponse
 
 class HomepageView(TemplateView):
     template_name = "about.html"
 
-#def index(request):
-#    return HttpResponse("Hello, world. This is the base of Judith's portfolio site.")
+    @staticmethod
+    def row_of_three_maker(queryset):
+        rows = list()
+        row = list()
+        print("QUERYSET AL PRINCIPIO", len(queryset))
+        for index, item in enumerate(queryset):
+            space = index % 4
+            print("SPACE IS: ", space)
+            if space == 0:
+                print("TRIGGERING ELSE")
+                row_to_copy = row.copy()
+                rows.append(row_to_copy)
+                row = list()
+            else:
+                row.append(item)
+        if len(row) != 0:
+            rows.append(row)
+        rows.pop(0)
+        return rows
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+
+        # Crear listas de 3 de los items en el portafolio para que podamos
+        # poner 3 por linea en la página
+        portfolio_queryset = PortfolioItem.objects.all()
+        list_of_rows = self.row_of_three_maker(portfolio_queryset)
+        for x in list_of_rows:
+            print("!! ROW", len(x))
+        data['portfolio_items'] = list_of_rows
+        return data
