626adbfb07cca598ad2c6f6cc3ad9a9e28843910
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2020-09-06 17:14:44 -0300

    disable certain admin features

--- judith/admin.py
+++ judith/admin.py
@@ -1,17 +1,27 @@
 from django.contrib import admin
 from judith.models import BioCastellano, BioIngles, HomePageItems, PortfolioItem, BlogPost, ContactInformation, EnglishPortfolioItem, CasetllanoPortfolioItem
 
-# Register your models here.
-admin.site.register(BioCastellano)
-admin.site.register(BioIngles)
-admin.site.register(HomePageItems)
-# admin.site.register(PortfolioItem)
-admin.site.register(EnglishPortfolioItem)
-admin.site.register(CasetllanoPortfolioItem)
-admin.site.register(BlogPost)
-admin.site.register(ContactInformation)
+
+
+class EditOnlyAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 admin.site.site_header = "Administración del Portfolio"
 admin.site.site_title = "Administración: Judith Morales"
 admin.site.index_title = "Bienvenida, Judith!"
+
+
+# Register your models here.
+admin.site.register(BioCastellano, EditOnlyAdmin)
+admin.site.register(BioIngles, EditOnlyAdmin)
+admin.site.register(HomePageItems, EditOnlyAdmin)
+# admin.site.register(PortfolioItem)
+admin.site.register(EnglishPortfolioItem)
+admin.site.register(CasetllanoPortfolioItem)
+admin.site.register(BlogPost)
+admin.site.register(ContactInformation, EditOnlyAdmin)
